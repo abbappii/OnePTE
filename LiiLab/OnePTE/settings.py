b67957e068db970e988f9cd406cd222489e2f2a0
abbappii <bappi142434@gmail.com>
2024-08-13 15:58:46 +0600

    added swagger

--- LiiLab/OnePTE/settings.py
+++ LiiLab/OnePTE/settings.py
@@ -60,7 +60,17 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     "rest_framework_simplejwt",
+    "drf_yasg",
 ]
+
+# swagger settings
+SWAGGER_SETTINGS = {
+    "SECURITY_DEFINITIONS": {"basic": {"type": "basic"}},
+}
+
+REDOC_SETTINGS = {
+    "LAZY_RENDERING": False,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- LiiLab/OnePTE/urls.py
+++ LiiLab/OnePTE/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -24,8 +24,38 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView,
 )
+from rest_framework import permissions
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="OnePTE",
+        default_version="main",
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Swagger
+    re_path(
+        r"^docs/swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=10),
+        name="schema-json",
+    ),
+    re_path(
+        r"^docs/swagger$",
+        schema_view.with_ui("swagger", cache_timeout=10),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^docs/redoc$",
+        schema_view.with_ui("redoc", cache_timeout=10),
+        name="schema-redoc",
+    ),
     # JWT Token
     path(
         "api/v1/token",
